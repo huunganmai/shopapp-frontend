5a95f176b604d6a416b9afaf2d1eebf319d3be9a
huunganmai <maihuungan3006@gmail.com>
2023-12-27 10:41:45 +0700

    feat: Enhance header component functionality and user interactive - Add poper and associated functionality to header _ Enhance header navigation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserResponse } from '../../response/user.response';
+import { UserService } from '../../services/user.service';
+import { TokenService } from '../../services/token.service';
 
 @Component({
     selector: 'app-header',
@@ -6,17 +10,51 @@ import { Component, OnInit } from '@angular/core';
     styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
+    userResponse?: UserResponse | null;
+    isPopoverOpen = false;
     activeNavItem: number = 0;
 
+    constructor(private router: Router, private userService: UserService, private tokenService: TokenService) {}
+
     ngOnInit(): void {
-        const storageNavItem = localStorage.getItem('active_nav_item');
-        if (storageNavItem) {
-            this.activeNavItem = parseInt(storageNavItem);
+        debugger;
+        this.userResponse = this.userService.getUserResponseFromLocalStorage();
+        const url = this.router.url;
+        switch (url) {
+            case '/':
+                this.activeNavItem = 0;
+                break;
+            case '/orders':
+                this.activeNavItem = 1;
+                break;
+            case '/notifications':
+                this.activeNavItem = 2;
+                break;
+            default:
+                this.activeNavItem = 0;
         }
     }
 
     setActiveNavItem(index: number) {
         this.activeNavItem = index;
-        localStorage.setItem('active_nav_item', JSON.stringify(index));
+    }
+
+    togglePopover(event: Event) {
+        event.preventDefault;
+        // this.activeNavItem = 3;
+        this.isPopoverOpen = !this.isPopoverOpen;
+    }
+
+    handleItemClick(index: number) {
+        if (index === 0) {
+            debugger;
+            this.router.navigate(['/user-profile']);
+        } else if (index === 2) {
+            this.userService.removeUserResponseFromLocalStorage();
+            this.tokenService.removeToken();
+            this.userService.getUserResponseFromLocalStorage();
+            this.userResponse = this.userService.getUserResponseFromLocalStorage();
+        }
+        this.isPopoverOpen = false;
     }
 }
