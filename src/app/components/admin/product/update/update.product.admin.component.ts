cf8616e37ecf00fec6e91111334a2591a6480f4c
huunganmai <maihuungan3006@gmail.com>
2024-01-23 16:38:30 +0700

    feat: Add update product admin page components - Added HTML structure for update product - Implemented TypeScript logic for update product - Styled the update product admin page with CSS

---
+++ src/app/components/admin/product/update/update.product.admin.component.ts
@@ -0,0 +1,151 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../../../models/product';
+import { Category } from '../../../../models/category';
+import { CategoryService } from '../../../../services/category.service';
+import { ProductService } from '../../../../services/product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UpdateProductDTO } from '../../../../dtos/products/update.product.dto';
+import { ProductImage } from '../../../../models/product.image';
+import { environment } from '../../../../environments/environment';
+
+@Component({
+    selector: 'app-update.product.admin',
+    templateUrl: './update.product.admin.component.html',
+    styleUrl: './update.product.admin.component.scss'
+})
+export class UpdateProductAdminComponent implements OnInit {
+    product: Product;
+    productId: number;
+    updatedProduct: Product;
+    categories: Category[] = [];
+    currentIndexImage: number = 0;
+    addedProductImage: File[] = [];
+    constructor(
+        private categoryService: CategoryService,
+        private productService: ProductService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) {
+        this.product = {} as Product;
+        this.updatedProduct = {} as Product;
+        this.productId = 0;
+    }
+
+    ngOnInit(): void {
+        debugger;
+        this.productId = Number(this.route.snapshot.paramMap.get('id'));
+        this.getDetailProduct();
+        this.getCategories(1, 100);
+    }
+
+    updateProduct() {
+        debugger;
+        const updateProductDTO: UpdateProductDTO = {
+            name: this.updatedProduct.name,
+            price: this.updatedProduct.price,
+            description: this.updatedProduct.description,
+            category_id: this.updatedProduct.category_id
+        };
+        this.productService.updateProduct(this.productId, updateProductDTO).subscribe({
+            next: (response: any) => {
+                debugger;
+            },
+            complete: () => {
+                debugger;
+                this.router.navigate(['/admin/products']);
+            },
+            error: (error: any) => {
+                debugger;
+                console.error("Can't update product: ", error);
+            }
+        });
+    }
+
+    getDetailProduct(): void {
+        debugger;
+        this.productService.getDetailProducts(this.productId).subscribe({
+            next: (response: Product) => {
+                this.product = response;
+                this.updatedProduct = { ...response };
+                this.updatedProduct.product_images.forEach(productImage => {
+                    productImage.image_url = `${environment.apiBaseUrl}/product_images/${productImage.image_url}`;
+                });
+                this.addedProductImage = [];
+            },
+            complete: () => {
+                debugger;
+            },
+            error: (error: any) => {
+                debugger;
+                console.error("Can't fetch product detail: ", error);
+            }
+        });
+    }
+
+    getCategories(page: number, limit: number) {
+        this.categoryService.getCategories(page, limit).subscribe({
+            next: (response: Category[]) => {
+                debugger;
+                this.categories = response;
+            },
+            complete: () => {
+                debugger;
+            },
+            error: (error: any) => {
+                debugger;
+                console.error("Can't fetch categories: ", error);
+            }
+        });
+    }
+
+    thumbnailClick(index: number, imageUrl: string) {
+        this.currentIndexImage = index;
+        this.product.thumbnail = imageUrl;
+    }
+
+    onFileChange(event: any) {
+        const files = event.target.files;
+        if (this.product.product_images.length + files.length > 5) {
+            alert('Maximum 5 images');
+            return;
+        }
+
+        this.productService.uploadImage(this.product.id, files).subscribe({
+            next: (productImages: ProductImage[]) => {
+                debugger;
+                productImages.forEach(productImage => {
+                    productImage.image_url = `${environment.apiBaseUrl}/product_images/${productImage.image_url}`;
+                });
+                this.product.product_images = [...this.product.product_images, ...productImages];
+                event.target.files = null;
+            },
+            complete: () => {
+                debugger;
+            },
+            error: (error: any) => {
+                debugger;
+                console.error("Can't upload images: ", error);
+            }
+        });
+    }
+
+    deleteImage(productImageDelete: ProductImage) {
+        if (confirm('Are you sure delete you want to remove this image?')) {
+            this.productService.deleteProductImage(productImageDelete.id).subscribe({
+                next: (response: any) => {
+                    debugger;
+                    this.product.product_images = this.product.product_images.filter(
+                        productImage => productImage.id !== productImageDelete.id
+                    );
+                },
+                complete: () => {
+                    debugger;
+                },
+                error: (error: any) => {
+                    debugger;
+                    console.error("Can't not delete product image: ", error);
+                }
+            });
+        }
+    }
+}
