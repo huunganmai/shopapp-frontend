50eb72d57b6b30a3a754fb766edabe65b2ccc392
huunganmai <maihuungan3006@gmail.com>
2024-01-23 16:36:25 +0700

    feat: Add product admin page components - Added HTML structure for insert product - Implemented TypeScript logic for create product - Styled the insert product admin page with CSS

---
+++ src/app/components/admin/product/insert/insert.product.admin.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit } from '@angular/core';
+import { InsertProductDTO } from '../../../../dtos/products/insert.product.dto';
+import { Category } from '../../../../models/category';
+import { CategoryService } from '../../../../services/category.service';
+import { ProductService } from '../../../../services/product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+    selector: 'app-insert.product.admin',
+    templateUrl: './insert.product.admin.component.html',
+    styleUrl: './insert.product.admin.component.scss'
+})
+export class InsertProductAdminComponent implements OnInit {
+    insertProductDTO: InsertProductDTO = {
+        name: '',
+        price: 0,
+        description: '',
+        category_id: 1,
+        images: []
+    };
+    categories: Category[] = [];
+
+    constructor(
+        private categoryService: CategoryService,
+        private productService: ProductService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) {}
+
+    ngOnInit(): void {
+        debugger;
+        this.getCategories(0, 12);
+    }
+
+    getCategories(page: number, limit: number) {
+        debugger;
+        this.categoryService.getCategories(page, limit).subscribe({
+            next: (response: Category[]) => {
+                debugger;
+                this.categories = response;
+            },
+            complete: () => {
+                debugger;
+            },
+            error: (error: any) => {
+                debugger;
+                console.error("Can't fetch categories: ", error);
+            }
+        });
+    }
+
+    onFileChange(event: any) {
+        let files = event.target.files;
+        if (files.length > 5) {
+            alert('Please select maximum of 5 files');
+            return;
+        }
+        this.insertProductDTO.images = files;
+    }
+
+    insertProduct() {
+        debugger;
+        this.productService.insertProduct(this.insertProductDTO).subscribe({
+            next: (response: any) => {
+                debugger;
+                if (this.insertProductDTO.images.length > 0) {
+                    this.productService.uploadImage(response.id, this.insertProductDTO.images).subscribe({
+                        next: (response: any) => {
+                            debugger;
+                            this.router.navigate(['..'], { relativeTo: this.route });
+                        },
+                        complete: () => {
+                            debugger;
+                        },
+                        error: (error: any) => {
+                            debugger;
+                            console.error("Can't upload image: ", error);
+                        }
+                    });
+                }
+            },
+            complete: () => {
+                debugger;
+            },
+            error: (error: any) => {
+                debugger;
+                console.error("Can't insert product: ", error);
+            }
+        });
+    }
+}
